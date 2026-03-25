06604960073be65230cbb8775bbced68ad2915c2
Biswajit-1-Das <biswajitdas90511@gmail.com>
2026-03-25 08:48:24 +0530

    Register added

---
+++ register.java
@@ -0,0 +1,5 @@
+public static void main (String args [])
+{
+    System.out.println("This is the code for the register file :: ");
+    System.out.println("--------------------------------------------");
+}
